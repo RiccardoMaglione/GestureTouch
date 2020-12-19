67a9314b8ddde94f6a7b9b83e602eec6eff63543
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-12-19 13:20:54 +0100

    Add spawner and sphere prefab

---
+++ Assets/Scripts/MovementSphere.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSphere : MonoBehaviour
+{
+    public int Speed = 5;
+    void Update()
+    {
+        if (this.gameObject.name == "GoUp") //Up to Down
+        {
+            transform.Translate(-transform.up * Speed * Time.deltaTime);
+        }
+        if (this.gameObject.name == "GoDown") //Down to Up
+        {
+            transform.Translate(transform.up * Speed * Time.deltaTime);
+        }
+        if (this.gameObject.name == "GoRight") //Right to Left
+        {
+            transform.Translate(transform.right * Speed * Time.deltaTime);
+        }
+        if (this.gameObject.name == "GoLeft") //Left to Right
+        {
+            transform.Translate(-transform.right * Speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject SphereGo;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 1, 3);
+    }
+
+    public void Spawn()
+    {
+        if(this.gameObject.name == "SpawnUp") //Up to Down
+        {
+            GameObject Go = Instantiate(SphereGo, new Vector3(Random.Range(-6, 7), transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 180));
+            Go.name = "GoUp";
+        }
+        if (this.gameObject.name == "SpawnDown") //Down to Up
+        {
+            GameObject Go = Instantiate(SphereGo, new Vector3(Random.Range(-6, 7), transform.position.y, transform.position.z), Quaternion.identity);
+            Go.name = "GoDown";
+        }
+        if (this.gameObject.name == "SpawnRight") //Right to Left
+        {
+            GameObject Go = Instantiate(SphereGo, new Vector3(transform.position.x, Random.Range(-8, 9), transform.position.z), Quaternion.Euler(0, 0, 90));
+            Go.name = "GoRight";
+        }
+        if (this.gameObject.name == "SpawnLeft") //Left to Right
+        {
+            GameObject Go = Instantiate(SphereGo, new Vector3(transform.position.x, Random.Range(-8, 9), transform.position.z), Quaternion.Euler(0, 0, 270));
+            Go.name = "GoLeft";
+        }
+    }
+}
