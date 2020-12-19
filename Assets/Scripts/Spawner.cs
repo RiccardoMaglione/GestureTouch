d9307374dee5fe819f210c9fcc14ca5836e25510
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-12-19 14:37:41 +0100

    Add shader

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,7 +8,7 @@ public class Spawner : MonoBehaviour
 
     void Start()
     {
-        InvokeRepeating("Spawn", 1, 3);
+        InvokeRepeating("Spawn", 1, 1);
     }
 
     public void Spawn()
@@ -17,21 +17,25 @@ public class Spawner : MonoBehaviour
         {
             GameObject Go = Instantiate(SphereGo, new Vector3(Random.Range(-6, 7), transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 180));
             Go.name = "GoUp";
+            Destroy(Go, 5);
         }
         if (this.gameObject.name == "SpawnDown") //Down to Up
         {
             GameObject Go = Instantiate(SphereGo, new Vector3(Random.Range(-6, 7), transform.position.y, transform.position.z), Quaternion.identity);
             Go.name = "GoDown";
+            Destroy(Go, 5);
         }
         if (this.gameObject.name == "SpawnRight") //Right to Left
         {
             GameObject Go = Instantiate(SphereGo, new Vector3(transform.position.x, Random.Range(-8, 9), transform.position.z), Quaternion.Euler(0, 0, 90));
             Go.name = "GoRight";
+            Destroy(Go, 5);
         }
         if (this.gameObject.name == "SpawnLeft") //Left to Right
         {
             GameObject Go = Instantiate(SphereGo, new Vector3(transform.position.x, Random.Range(-8, 9), transform.position.z), Quaternion.Euler(0, 0, 270));
             Go.name = "GoLeft";
+            Destroy(Go, 5);
         }
     }
 }
